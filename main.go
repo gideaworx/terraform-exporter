93f93b0bda6049502410a576e09ec8588e46aebe
Josh Ghiloni <josh.ghiloni@move.com>
2023-04-27 09:53:08 -0400

    Add version info

--- main.go
+++ main.go
@@ -11,6 +11,8 @@ import (
 	"github.com/gideaworx/terraform-exporter/registry"
 )
 
+var Version = "0.0.0-local"
+
 var cli struct {
 	Export        *export.Command            `cmd:"" help:"Export data to terraform files"`
 	InstallPlugin *install.Command           `cmd:"" help:"Install a plugin"`
@@ -18,10 +20,13 @@ var cli struct {
 	ListPlugins   *list.ListPluginsCommand   `cmd:"" help:"List installed plugins"`
 	ListCommands  *list.ListExportersCommand `cmd:"" help:"List commands provided by installed plugins"`
 	Registry      *registry.Command          `cmd:"" help:"Work with plugin registries"`
+	Version       kong.VersionFlag           `short:"v" optional:"true" help:"Show the version and quit"`
 }
 
 func main() {
-	ctx := kong.Parse(&cli)
+	ctx := kong.Parse(&cli, kong.Vars{
+		"version": Version,
+	})
 	ctx.BindTo(ctx.Stdout, (*io.Writer)(nil))
 	ctx.FatalIfErrorf(ctx.Run())
 }
